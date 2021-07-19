c8e3302e7eda17ace015c81ff6fa00fce838c6ab
Olsson <78812304+olssonik@users.noreply.github.com>
2021-07-19 22:21:51 +0100

    Add files via upload

--- easyonionsite.sh
+++ easyonionsite.sh
@@ -1,4 +1,28 @@
-#$bin/bash
-x=0
-while [ $x = 0 ]
-do
+        clear
+        echo "
+ _____    _    ______   __   ___  _   _ ___ ___  _   _ 
+| ____|  / \  / ___\ \ / /  / _ \| \ | |_ _/ _ \| \ | |
+|  _|   / _ \ \___ \\  V /  | | | |  \| || | | | |  \| |
+| |___ / ___ \ ___) || |   | |_| | |\  || | |_| | |\  |
+|_____/_/   \_\____/ |_|    \___/|_| \_|___\___/|_| \_|
+                                                       
+
+"
+
+
+# glowny przypadek
+
+	sudo apt install tor -y && apt install nginx -y
+	sudo service nginx start
+	sudo service tor start
+	#kopiowanie configu na miejsce aktualnej konfiguracji
+	sudo cp torrc /etc/tor/
+	sudo cp nginx.conf /etc/nginx/
+	#restart servisow
+	sudo service tor restart && sudo service nginx restart
+	echo "give it a second now cuz script will restart tor and nginx service, we are getting there"
+	sleep 10
+	#adres twojej strony
+	echo "And here is Your ready to go onion site"
+	sudo cat /var/lib/tor/hidden_service/hostname
+
