17612aacb55989b85de3fa897d5f2e7b7908ab4e
Olsson <78812304+olssonik@users.noreply.github.com>
2021-08-13 15:44:10 +0100

    Update easyonionsite.sh

--- easyonionsite.sh
+++ easyonionsite.sh
@@ -16,8 +16,8 @@
 	sudo service nginx start
 	sudo service tor start
 	#kopiowanie configu na miejsce aktualnej konfiguracji
-	sudo cp torrc /etc/tor/
-	sudo cp nginx.conf /etc/nginx/
+	sudo cp /conf_files/torrc /etc/tor/
+	sudo cp /conf_files/nginx.conf /etc/nginx/
 	#restart servisow
 	sudo service tor restart && sudo service nginx restart
 	clear
@@ -26,4 +26,3 @@
 	#adres twojej strony
 	echo "And here is Your ready to go onion site"
 	sudo cat /var/lib/tor/hidden_service/hostname
-
